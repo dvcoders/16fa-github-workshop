10b80c76a8a2143a68c42e6960125d9c4c2fd9dd
MockingJ <MockingJ@users.noreply.github.com>
2016-09-26 18:43:33 -0700

    Update infiniteLoop.cpp

--- infiniteLoop.cpp
+++ infiniteLoop.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-	int i = 0;
-
-	for (int i = 0; i < 5; i++) {
+int main () 
+{
+	
+	for (int i = 0; i < 5; i++) 
+	{
 		cout << "Hello World!" << endl;
 	}
 	return 0;
